6185707d36245855e4a406aebaa9b8dcfc1333d0
Steven <fernandezklein123@gmail.com>
2026-06-13 20:25:27 +0800

    Simple Profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { useState } from "react";
 
 function App() {
 
-  const [name , setName] = useState("");
-
+  const [name , setName] = useState("Steven");
+  const [age , setAge] = useState(20);
+  
+  const incrementAge = () => {
+    setAge((prevState) => prevState + 1);
+  }
+  const decrementAge = () => {
+    setAge((prevState) => prevState > 0 ? prevState - 1 : 0);
+  }
   return (
     <>  
-
-      <input
-        type="text"
-        placeholder="Enter name..."
-        className="border border-gray-400 px-3 py-2 rounded" onChange={(e) => setName(e.target.value)}
-      />
-      
-
-      <h1>{name ? `Hello ${name}` : "Please enter your name."}</h1>
+    <div className="bg-gray-700 w-64 h-32 text-white flex flex-col justify-center items-center">
+        <h1>Name : {name}</h1>
+        <p>Age : {age}</p>
+          <div className="flex flex-row gap-2">
+            <button className="bg-purple-600 rounded-xl px-2 my-2 cursor-pointer" onClick={incrementAge}>Increment</button>
+            <button className="bg-purple-600 rounded-xl px-2 my-2 cursor-pointer" onClick={decrementAge}>Decrement</button>
+          </div>
+    </div>
     </>
   );  
 }
