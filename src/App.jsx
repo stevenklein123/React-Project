1de6adb487e58e28968b9541f93d6eb5193dab93
Steven <fernandezklein123@gmail.com>
2026-06-20 17:10:05 +0800

    Create a Todo List but Add and Delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,62 @@
 import { useState } from "react";
 
-
 function App() {
 
-  const [like , setLike] = useState(0);
-  const [disLike , setDislike] = useState(0);
+  const [inputTask , setInputTask] = useState("");
+  const [task, setTask] = useState([]);
 
-  const IncrementLike = () => {
-    setLike((prevState) => prevState+1);
+
+  const addTask = () => {
+
+      if(inputTask === "") return;
+      setTask((prev) => [...prev , inputTask]);
+      
+      setInputTask("");
   }
 
-    const IncrementDisLike = () => {
-    setDislike((prevState) => prevState+1);
-  }
-
-  const resetFunc = () => { 
-    setLike(0);
-    setDislike(0);
+  const deleteTask = (index) => {
+      setTask((prev) => prev.filter((_ , i) => i !== index))
   }
 
 
+  const updateTask = (index) => {
+    setTask((prev) => prev.map((task , i) => (
+      i === index ? <input type="text" placeholder="Enter Update Task"></input> : task
+    ))) }
+  
   return (
     <>  
-<div className="min-h-screen flex justify-center items-center">
 
-  <div className="
-    text-sm md:text-lg lg:text-xl
-    bg-slate-700 text-white
-    w-full max-w-xs
-    p-4
-    flex flex-col
-    rounded-xl
-  ">
+      <div className="min-h-screen bg-white-700 text-white flex flex-col justify-center items-center">
 
-    <p>Like : {like}</p>
-    <p>👍</p>
+        <div className="bg-slate-950 w-150 h-150 rounded-3xl shadow-lg border-black">
 
-    <button
-      className="bg-purple-700 rounded-xl py-2 my-2 cursor-pointer"
-      onClick={IncrementLike}
-    >
-      Like
-    </button>
+          <h1 className="font-sans font-bold flex flex-col justify-center items-center py-5 text-3xl">Todo List</h1>
 
-    <p>Dislike : {disLike}</p>
-    <p>👎</p>
+          <input type="text" placeholder=" Enter Task..." className="border py-2 mx-10 my-10 rounded-md" value={inputTask} onChange={(e) => setInputTask(e.target.value)} />
 
-    <button
-      className="bg-purple-700 rounded-xl py-2 my-2 cursor-pointer"
-      onClick={IncrementDisLike}
-    >
-      Dislike
-    </button>
+          <button className="bg-blue-400 rounded-xl p-2 cursor-pointer" onClick={addTask}>Add task</button>
 
-    <button
-      className="bg-blue-700 rounded-xl py-2 my-2 cursor-pointer"
-      onClick={resetFunc}
-    >
-      Reset
-    </button>
+          {task.map((item, index) => (
+            <div key={index} className="flex justify-between items-center mx-10 my-2">
+              
+              {/* Task text on the left */}
+              <span className="font-bold">
+                {index + 1} : {item}
+              </span>
 
-  </div>
+              <div className="flex gap-2"> 
+                <button className="bg-red-400 rounded-xl p-2 cursor-pointer" onClick={()=> deleteTask(index)}>Delete</button>
+              </div>
+              
+            </div>
+          ))}
 
-</div>
+
+
+        </div>
+
+      </div>
 
     </>
   );  
